f7ac88439406848b0e3345496643dc9c1eb0f90b
Pierre Qi <qiqinyuan@gmail.com>
2023-09-14 19:31:55 +0200

    Add package document

---
+++ R/lab03-package.R
@@ -0,0 +1,13 @@
+#' Lab 3 of Advanced Programming in R
+#'
+#' This package include following functions.\cr 
+#' 1. euclidean\cr 
+#' 2. dijkstra\cr 
+#' \cr
+#' The package also has the following data sources included.\cr 
+#' 1. wiki_graph \cr 
+
+#' @name lab03-package
+#' @aliases lab03
+#' @docType package
+NULL
