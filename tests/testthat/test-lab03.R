683c4f52edf12536a8b32d8d43e3688fa24e58af
sakaushikb4u <76866642+sakaushikb4u@users.noreply.github.com>
2023-09-14 12:12:23 +0200

    Update test-lab03.R

--- tests/testthat/test-lab03.R
+++ tests/testthat/test-lab03.R
@@ -15,4 +15,7 @@ test_that("Test for dijkstra function using sample input 02", {
   result <- dijkstra(wiki_graph,3)
   expect_equal(result, c(9,10,0,11,11,2))
 })
-
+test_that("Test for euclidean function", {
+  result <- euclidean(10, 20)
+  expect_equal(result, 10)
+})
