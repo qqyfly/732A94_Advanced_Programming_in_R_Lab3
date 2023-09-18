c3b63fbe72a8966dad0f423126a030d28f9e3959
SATYA SAI NAGA JAYA KOUSHIK PILLA <76866642+sakaushikb4u@users.noreply.github.com>
2023-09-18 21:47:49 +0200

    Update wiki_graph.R

--- R/wiki_graph.R
+++ R/wiki_graph.R
@@ -3,7 +3,7 @@
 #' @format A data frame contain the edges between nodes and distance of those edges.
 #' \describe{
 #'   \item{v1}{start_node}
-#'   \item{v2}{end node}
+#'   \item{v2}{end_node}
 #'   \item{w}{edge length}
 #' }
 "wiki_graph"
