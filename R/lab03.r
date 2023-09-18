e653676a837a30d2819fd0d356490d2c8c45520c
SATYA SAI NAGA JAYA KOUSHIK PILLA <76866642+sakaushikb4u@users.noreply.github.com>
2023-09-18 21:25:16 +0200

    Update lab03.r

--- R/lab03.r
+++ R/lab03.r
@@ -101,12 +101,4 @@ dijkstra <- function(graph, init_node) {
   return(dist)
 }
 
-#' wiki_graph:  data frame for dijkstra function
-#'
-#' @format A data frame contain the edges between nodes and distance of those edges.
-#' \describe{
-#'   \item{v1}{start node}
-#'   \item{v2}{end node}
-#'   \item{w}{edge length}
-#' }
-"wiki_graph"
+
